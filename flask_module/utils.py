3b9d1d6f60f3898eeb7fd5fa50bab61c18aec069
PodiWan <tpodina@gmail.com>
2021-11-17 10:47:58 +0200

    feat: add flask-socketio to allow access over localhost

--- flask_module/utils.py
+++ flask_module/utils.py
@@ -1,15 +1,22 @@
+import asyncio
 from opencv_module import utils
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit, disconnect
+
 app = Flask(__name__)
+async_mode = None
+socket_ = SocketIO(app, async_mode=async_mode)
 
 @app.route('/video-feed')
 def video_feed():
-    return Response(utils.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    video_response = Response(utils.generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    # SocketIO.emit('video', video_response, broadcast=True)
+    return video_response
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', async_mode=socket_.async_mode)
 
 def flask_start():
     print(f'Starting...')
-    app.run(debug=True)
+    socket_.run(app=app, host='0.0.0.0', port=5000)
